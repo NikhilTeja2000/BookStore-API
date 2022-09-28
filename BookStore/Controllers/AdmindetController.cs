7aa28c30cca0bfca2b23620b4d4d76274f5d10d4
NikhilTeja2000 <83840038+NikhilTeja2000@users.noreply.github.com>
2022-09-28 04:42:28 -0700

    Add files via upload

---
+++ BookStore/Controllers/AdmindetController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class AdmindetController : ApiController
+    {
+        private IAdmindetRepository repository;
+
+        public AdmindetController()
+        {
+            repository = new AdmindetSqlImpl();
+        }
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var data = repository.GetAllAdmindet();
+            return Ok(data);
+        }
+    }
+}
---
+++ BookStore/Controllers/UserdetController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class UserdetController : ApiController
+    {
+        private IUserdetRepository repository;
+        public UserdetController()
+        {
+            repository = new UserdetSqlImpl();
+        }
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var data = repository.GetAllUserdet();
+            return Ok(data);
+        }
+    }
+}
