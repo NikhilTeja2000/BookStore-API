7ecd55e77694cd76140d69fe9718d2d38c6b2dbe
NikhilTeja2000 <83840038+NikhilTeja2000@users.noreply.github.com>
2022-09-29 01:02:53 -0700

    Add files via upload

---
+++ BookStore/Controllers/UserCartController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using BookStore.Models;
+namespace BookStore.Controllers
+{
+    public class UserCartController : ApiController
+    {
+        private IUserCartRepository repository;
+        public UserCartController()
+        {
+            repository = new UserCartSqlImpl();
+        }
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var data = repository.GetAllUserCart();
+            return Ok(data);
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var data = repository.DeleteUserCart(id);
+            return Ok(data);
+        }
+    }
+}
---
+++ BookStore/Controllers/UserOrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class UserOrderController : ApiController
+    {
+
+        private IUserOrderRepository repository;
+        public UserOrderController()
+        {
+            repository = new UserOrderSqlImpl();
+        }
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var data = repository.GetAllUserOrder();
+            return Ok(data);
+        }
+    }
+}
